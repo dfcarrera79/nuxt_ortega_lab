e53d3b78e988d1242529f6dd081eb982a27623a4
dfcarrera79 <44879886+dfcarrera79@users.noreply.github.com>
2024-01-20 09:30:07 -0500

    Update package-lock.json and app.scss

---
+++ components/FooterComponent.vue
@@ -0,0 +1,106 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div class="column background full-width">
+    <div
+      class="row justify-around items-center content-center"
+      style="height: 150px"
+    >
+      <div class="text-white">
+        <div class="text-h4" style="font-family: 'Lato'">
+          Ortega Pathology Lab
+        </div>
+        <hr />
+
+        <div class="text-subtitle1 et_pb_text_1">
+          Tu salud, nuestro compromiso.
+        </div>
+      </div>
+      <div class="row justify-center">
+        <q-btn
+          flat
+          size="lg"
+          target="_blank"
+          href="https://www.facebook.com/LabPath"
+        >
+          <q-avatar>
+            <img src="../assets/facebook.svg" />
+          </q-avatar>
+          <q-tooltip
+            transition-show="scale"
+            transition-hide="scale"
+            anchor="bottom middle"
+            self="top middle"
+          >
+            <span class="et_pb_text_1">Ir a Facebook</span>
+          </q-tooltip>
+        </q-btn>
+        <q-btn
+          flat
+          size="lg"
+          target="_blank"
+          href="https://www.instagram.com/ortega_pathology/"
+        >
+          <q-avatar>
+            <img src="../assets/instagram.svg" />
+          </q-avatar>
+          <q-tooltip
+            transition-show="scale"
+            transition-hide="scale"
+            anchor="bottom middle"
+            self="top middle"
+          >
+            <span class="et_pb_text_1">Ir a Instagram</span>
+          </q-tooltip>
+        </q-btn>
+      </div>
+    </div>
+
+    <div>
+      <div class="row justify-evenly">
+        <p class="text-caption text-white et_pb_text_1">
+          Copyright &copy; 2024 OrtegaLab
+        </p>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.et_pb_text_1 {
+  line-height: 1.6em;
+  font-family: "Josefin Sans", Helvetica, Arial, Lucida, sans-serif;
+  font-weight: 300;
+  font-size: 18px;
+  line-height: 1.6em;
+}
+.custom-caption {
+  text-align: left !important;
+  font-size: 48px !important;
+  line-height: 1.1em !important;
+  text-transform: uppercase;
+  color: #fff;
+  font-weight: 400;
+  padding: 12px;
+  transition: transform 0.7s ease 0.5s;
+  transform: translateX(1400px);
+  opacity: 0;
+}
+
+.background {
+  background: linear-gradient(45deg, #333333, #666666);
+
+  /* Establecer un degradado desvanecido hacia abajo */
+  background: linear-gradient(to bottom, #333333, #666666);
+
+  /* Añadir una transición suave para el desvanecido */
+  transition: background 0.5s ease;
+}
+
+hr {
+  border: none;
+  height: 0.75px;
+  background-color: white;
+  width: 100%;
+}
+</style>
--- layouts/default.vue
+++ layouts/default.vue
@@ -1,211 +1,275 @@
 <script setup lang="ts">
-import { ref } from "vue";
 import { useQuasar } from "quasar";
+import Foot from "../components/FooterComponent.vue";
+import { ref, onMounted, onBeforeUnmount } from "vue";
 
 // Data
-const miniState = ref(true);
-const { dark } = useQuasar();
-const leftDrawerOpen = ref(false);
+const $q = useQuasar();
+const scrolled = ref(false);
+const isHovered = ref(false);
+const rightDrawerOpen = ref(false);
+const menuList = [
+  {
+    icon: "home",
+    label: "Inicio",
+    separator: false,
+    to: "/",
+  },
+  {
+    icon: "medical_services",
+    label: "Servicios",
+    separator: false,
+    to: "#/servicios",
+  },
+  {
+    icon: "group",
+    label: "Nosotros",
+    separator: false,
+    to: "#/nosotros",
+  },
+  {
+    icon: "alternate_email",
+    label: "Contacto",
+    separator: false,
+    to: "#/contacto",
+  },
+  {
+    icon: "article",
+    label: "Resultados Online",
+    separator: false,
+    to: "http://ortegalabcloud.com/",
+  },
+];
 
 // Methods
-function toggleLeftDrawer() {
-  leftDrawerOpen.value = !leftDrawerOpen.value;
-}
+const toggleRightDrawer = () => {
+  rightDrawerOpen.value = !rightDrawerOpen.value;
+};
+
+const handleMouseEnter = () => {
+  isHovered.value = true;
+};
+
+const handleMouseLeave = () => {
+  isHovered.value = false;
+};
+
+// Función para manejar el evento de desplazamiento
+const handleScroll = () => {
+  scrolled.value = window.scrollY > 0;
+};
+
+// Agregar y quitar el listener del evento de desplazamiento
+onMounted(() => {
+  window.addEventListener("scroll", handleScroll);
+});
+
+onBeforeUnmount(() => {
+  window.removeEventListener("scroll", handleScroll);
+});
 </script>
+
 <template>
-  <q-layout view="lHh Lpr lFf">
-    <q-header>
+  <q-layout view="hHr lpR fFf">
+    <q-page-container style="margin-top: -91px">
+      <router-view />
+    </q-page-container>
+
+    <q-header class="text-white background-none">
       <q-toolbar>
-        <q-btn
-          flat
-          dense
-          round
-          icon="menu"
-          aria-label="Menu"
-          @click="toggleLeftDrawer"
-        />
-
-        <q-toolbar-title
-          class="text-h4 row text-uppercase"
-          style="font-family: 'Bebas Neue'"
-        >
-          Control de asistencia
+        <q-toolbar-title>
+          <a href="/">
+            <img
+              ref="logo"
+              src="../assets/logo.png"
+              class="q-pa-sm q-transition--fast"
+              :style="{ height: scrolled ? '50px' : '70px' }"
+              alt="Descripción de la imagen"
+            />
+          </a>
         </q-toolbar-title>
-
-        <div class="row items-center content-center q-mr-md">
-          <q-toggle
-            :model-value="dark.isActive"
-            checked-icon="dark_mode"
-            unchecked-icon="light_mode"
-            size="3rem"
-            @update:model-value="(val) => dark.set(val)"
-          />
-          <q-btn flat dense>
-            <div class="q-mr-sm" v-if="!($q.screen.lt.md || $q.screen.lt.sm)">
-              Salir
-            </div>
-            <q-icon left name="logout" />
+        <q-space />
+        <div class="q-pr-md" v-if="!($q.screen.lt.md || $q.screen.lt.sm)">
+          <q-btn
+            outline
+            color="white"
+            no-caps
+            label="Resultados Online"
+            class="custom-btn et_pb_text_1"
+            href="http://ortegalabcloud.com/"
+          >
+            <q-icon left size="2em" name="chevron_right" />
           </q-btn>
         </div>
+        <q-btn
+          dense
+          flat
+          round
+          icon="menu"
+          :color="isHovered ? 'primary' : 'white'"
+          @click="toggleRightDrawer"
+          @mouseenter="handleMouseEnter"
+          @mouseleave="handleMouseLeave"
+        />
       </q-toolbar>
     </q-header>
 
     <q-drawer
-      v-model="leftDrawerOpen"
       show-if-above
-      bordered
-      :width="320"
-      :mini="miniState"
-      @mouseover="miniState = false"
-      @mouseout="miniState = true"
+      v-model="rightDrawerOpen"
+      side="right"
+      behavior="mobile"
+      style="background-color: #191919"
     >
-      <q-img
-        fit="fill"
-        class="absolute-top q-pa-sm"
-        src="../assets/loxasoluciones.png"
-        style="height: 150px"
-        v-show="!miniState"
-      />
-      <q-scroll-area
-        style="
-          height: calc(100% - 150px);
-          margin-top: 150px;
-          border-right: 1px solid #ddd;
-        "
-      >
-        <q-list padding>
-          <q-item class="column" v-show="!miniState">
-            <q-separator inset />
-            <div style="width: 300px; white-space: wrap; overflow: hidden">
-              <div class="text-center">Bienvenido(a)</div>
-              <!-- <div class="text-weight-bold text-center">
-              {{ sessionData.usuario }}
-            </div> -->
-            </div>
-            <q-separator inset />
-          </q-item>
+      <q-scroll-area class="fit text-white et_pb_text_1">
+        <q-list>
+          <template v-for="(menuItem, index) in menuList" :key="index">
+            <q-item
+              clickable
+              :href="menuItem.to"
+              :active="menuItem.label === 'Outbox'"
+              v-ripple
+            >
+              <q-item-section avatar>
+                <q-icon :name="menuItem.icon" size="sm" />
+              </q-item-section>
+              <q-item-section>
+                {{ menuItem.label }}
+              </q-item-section>
+            </q-item>
+            <q-separator :key="'sep' + index" v-if="menuItem.separator" />
+          </template>
+          <q-expansion-item expand-separator icon="language" label="WebLAB">
+            <q-list>
+              <q-item clickable v-ripple href="http://186.5.111.32:9595/sage/">
+                <q-item-section avatar>
+                  <q-icon name="atr" />
+                </q-item-section>
+                <q-item-section> Financiero </q-item-section>
+              </q-item>
 
-          <q-item clickable v-ripple to="/" active-class="my-menu-link">
-            <q-item-section avatar>
-              <q-icon name="home" />
-            </q-item-section>
-
-            <q-item-section>
-              <span class="text-h6" style="font-family: 'Bebas Neue'">
-                PÁGINA DE INICIO
-              </span>
-            </q-item-section>
-          </q-item>
-
-          <q-separator />
-
-          <q-item
-            clickable
-            v-ripple
-            to="/gestion_usuarios"
-            active-class="my-menu-link"
-          >
-            <q-item-section avatar>
-              <q-icon name="people_alt" />
-            </q-item-section>
-
-            <q-item-section>
-              <span class="text-h6" style="font-family: 'Bebas Neue'">
-                GESTIÓN DE EMPLEADOS
-              </span>
-            </q-item-section>
-          </q-item>
-
-          <q-item
-            clickable
-            v-ripple
-            to="/registros_biometricos"
-            active-class="my-menu-link"
-          >
-            <q-item-section avatar>
-              <q-icon name="person_pin" />
-            </q-item-section>
-
-            <q-item-section>
-              <span class="text-h6" style="font-family: 'Bebas Neue'">
-                REGISTROS BIOMÉTRICOS
-              </span>
-            </q-item-section>
-          </q-item>
-
-          <q-item
-            clickable
-            v-ripple
-            to="/configuracion_horarios"
-            active-class="my-menu-link"
-          >
-            <q-item-section avatar>
-              <q-icon name="schedule" />
-            </q-item-section>
-
-            <q-item-section>
-              <span class="text-h6" style="font-family: 'Bebas Neue'">
-                CONFIGURACIÓN DE HORARIOS
-              </span>
-            </q-item-section>
-          </q-item>
-
-          <q-item
-            clickable
-            v-ripple
-            to="/gestion_exepciones"
-            active-class="my-menu-link"
-          >
-            <q-item-section avatar>
-              <q-icon name="ballot" />
-            </q-item-section>
-
-            <q-item-section>
-              <span class="text-h6" style="font-family: 'Bebas Neue'">
-                GESTIÓN DE EXEPCIONES
-              </span>
-            </q-item-section>
-          </q-item>
-
-          <q-item
-            clickable
-            v-ripple
-            to="/horas_suplementarias"
-            active-class="my-menu-link"
-          >
-            <q-item-section avatar>
-              <q-icon name="more_time" />
-            </q-item-section>
-
-            <q-item-section>
-              <span class="text-h6" style="font-family: 'Bebas Neue'">
-                Registro de horas suplementarias
-              </span>
-            </q-item-section>
-          </q-item>
-
-          <q-item
-            clickable
-            v-ripple
-            to="/registro_asistencias"
-            active-class="my-menu-link"
-          >
-            <q-item-section avatar>
-              <q-icon name="how_to_reg" />
-            </q-item-section>
-
-            <q-item-section>
-              <span class="text-h6" style="font-family: 'Bebas Neue'">
-                REGISTRO DE ASISTENCIAS
-              </span>
-            </q-item-section>
-          </q-item>
+              <q-item clickable v-ripple href="http://186.5.111.32:3000">
+                <q-item-section avatar>
+                  <q-icon name="atr" />
+                </q-item-section>
+                <q-item-section> Pneuma </q-item-section>
+              </q-item>
+            </q-list>
+          </q-expansion-item>
         </q-list>
       </q-scroll-area>
+      <div class="absolute" style="top: 15px; right: 10px">
+        <q-btn
+          dense
+          round
+          flat
+          :color="isHovered ? 'primary' : 'white'"
+          unelevated
+          icon="close"
+          @click="rightDrawerOpen = false"
+          @mouseenter="handleMouseEnter"
+          @mouseleave="handleMouseLeave"
+        />
+      </div>
     </q-drawer>
 
-    <q-page-container>
-      <slot />
-    </q-page-container>
+    <q-page-scroller
+      position="bottom-right"
+      :scroll-offset="150"
+      :offset="[18, 18]"
+    >
+      <q-btn icon="keyboard_arrow_up" outline round color="white">
+        <q-tooltip
+          transition-show="scale"
+          transition-hide="scale"
+          anchor="center right"
+          self="center left"
+        >
+          <span class="et_pb_text_1" style="white-space: nowrap"
+            >Ir al inicio</span
+          >
+        </q-tooltip>
+      </q-btn>
+    </q-page-scroller>
+
+    <div
+      :class="
+        !scrolled
+          ? 'fixed-bottom-right fixed-whatsapp-btn'
+          : 'fixed-bottom-right whatsapp-btn'
+      "
+    >
+      <q-btn
+        flat
+        round
+        size="md"
+        target="_blank"
+        href="https://wa.me/593995984963"
+      >
+        <q-avatar>
+          <img src="../assets/whatsapp.svg" />
+        </q-avatar>
+        <q-tooltip
+          transition-show="scale"
+          transition-hide="scale"
+          anchor="center right"
+          self="center left"
+        >
+          <span class="et_pb_text_1" style="white-space: nowrap"
+            >Escríbenos por WhatsApp</span
+          >
+        </q-tooltip>
+      </q-btn>
+    </div>
+
+    <Foot />
   </q-layout>
 </template>
+
+<style lang="scss">
+.et_pb_text_1 {
+  line-height: 1.6em;
+  font-family: "Josefin Sans", Helvetica, Arial, Lucida, sans-serif;
+  font-weight: 400;
+  font-size: 18px;
+  line-height: 1.6em;
+}
+
+.whatsapp-btn {
+  margin-right: 15px;
+  margin-bottom: 70px;
+  transition: transform 0.5s ease;
+}
+
+.fixed-whatsapp-btn {
+  margin-right: 15px;
+  margin-bottom: 70px;
+  transform: translateY(40px); /* Adjust the initial position */
+}
+
+.background-none {
+  background: rgba(222, 218, 218, 0);
+}
+.q-transition--fast {
+  transition: height 0.4s ease; /* Ajusta la duración y el tipo de animación según tus preferencias */
+}
+
+.custom-btn:hover .q-icon {
+  animation: moveIconToRight 0.1s ease-in-out;
+  animation-delay: 50ms;
+  animation-fill-mode: forwards;
+}
+
+@keyframes moveIconToRight {
+  0% {
+    transform: translateX(0px);
+  }
+  100% {
+    transform: translateX(8px);
+  }
+}
+
+.custom-btn:not(:hover) .q-icon {
+  display: none;
+}
+</style>
