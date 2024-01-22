5014329896e08a4b8f78e9c06feb8a363e700284
dfcarrera79 <44879886+dfcarrera79@users.noreply.github.com>
2024-01-22 08:47:55 -0500

    Refactor form.vue component

--- components/contacto/form.vue
+++ components/contacto/form.vue
@@ -1,6 +1,6 @@
 <script setup lang="ts">
-import { ref } from 'vue';
-import { useQuasar } from 'quasar';
+import { ref } from "vue";
+import { useQuasar } from "quasar";
 
 // Data
 const $q = useQuasar();
@@ -9,17 +9,17 @@ const name = ref(null);
 const email = ref(null);
 const telefono = ref(null);
 const asunto = ref(null);
-const editor = ref('Mensaje: ');
+const editor = ref("Mensaje: ");
 
 // Methods
 const onSubmit = () => {
   // Validate inputs
   if (!name.value || !email.value || !telefono.value || !asunto.value) {
     $q.notify({
-      color: 'negative',
-      textColor: 'white',
-      icon: 'warning',
-      message: 'Todos los campos son obligatorios',
+      color: "negative",
+      textColor: "white",
+      icon: "warning",
+      message: "Todos los campos son obligatorios",
     });
     return;
   }
@@ -41,64 +41,80 @@ const onSubmit = () => {
 </script>
 
 <template>
-  <div class="q-pa-md" style="max-width: 400px">
+  <div
+    class="q-pa-md fit row wrap justify-around items-center content-center"
+    style="max-width: 800px"
+  >
     <q-form @submit="onSubmit" class="q-gutter-md">
       <div class="column justify-center">
         <div class="row justify-center q-gutter-md">
-          <q-input
-            dark
-            v-model="name"
-            label="Nombre"
-            lazy-rules
-            :rules="[
-              (val) => (val && val.length > 0) || 'Por favor escriba su nombre',
-            ]"
-          />
-          <q-input
-            dark
-            v-model="telefono"
-            label="Teléfono"
-            lazy-rules
-            :rules="[
-              (val) => !!val || 'Please enter your phone number',
-              (val) =>
-                /^[0-9]{10}$/.test(val) ||
-                'Número de teléfono inválido (10 dígitos))',
-            ]"
-          />
+          <div style="width: 290px">
+            <q-input
+              dark
+              v-model="name"
+              label="Nombre"
+              lazy-rules
+              :rules="[
+                (val) =>
+                  (val && val.length > 0) || 'Por favor escriba su nombre',
+              ]"
+            />
+          </div>
+          <div style="width: 290px">
+            <q-input
+              dark
+              v-model="telefono"
+              label="Teléfono"
+              lazy-rules
+              :rules="[
+                (val) => !!val || 'Por favor ingrese su número de teléfono',
+                (val) =>
+                  /^[0-9]{10}$/.test(val) ||
+                  'Número de teléfono inválido (10 dígitos))',
+              ]"
+            />
+          </div>
         </div>
 
         <div class="row justify-center q-gutter-md">
-          <q-input
-            dark
-            v-model="email"
-            label="Email"
-            lazy-rules
-            :rules="[
-              (val) => !!val || 'Please enter your email address',
-              (val) =>
-                /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(val) ||
-                'Invalid email address',
-            ]"
-          />
-          <q-input
-            dark
-            v-model="asunto"
-            label="Asunto"
-            lazy-rules
-            :rules="[
-              (val) => (val && val.length > 0) || 'Por favor escriba un asunto',
-            ]"
-          />
+          <div style="width: 290px">
+            <q-input
+              dark
+              v-model="email"
+              label="Email"
+              lazy-rules
+              :rules="[
+                (val) => !!val || 'Por favor ingrese su correo',
+                (val) =>
+                  /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(val) ||
+                  'Dirección de correo inválida',
+              ]"
+            />
+          </div>
+          <div style="width: 290px">
+            <q-input
+              dark
+              v-model="asunto"
+              label="Asunto"
+              lazy-rules
+              :rules="[
+                (val) =>
+                  (val && val.length > 0) || 'Por favor escriba un asunto',
+              ]"
+            />
+          </div>
         </div>
 
-        <div class="q-pa-md">
-          <q-editor v-model="editor" min-height="5rem" />
+        <div class="q-pa-md" style="min-width: 650px">
+          <q-editor
+            v-model="editor"
+            min-height="8rem"
+            :content-style="{ backgroundColor: 'rgba(192, 192, 192, 1)' }"
+          />
         </div>
       </div>
 
       <div>
-        <!-- <q-btn label="Enviar" type="submit" color="primary" /> -->
         <q-btn
           outline
           color="white"
@@ -118,7 +134,7 @@ const onSubmit = () => {
 <style lang="scss" scoped>
 .et_pb_text_1 {
   line-height: 1.6em;
-  font-family: 'Josefin Sans', Helvetica, Arial, Lucida, sans-serif;
+  font-family: "Josefin Sans", Helvetica, Arial, Lucida, sans-serif;
   font-weight: 300;
   font-size: 18px;
   line-height: 1.6em;
